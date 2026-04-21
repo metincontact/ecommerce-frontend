ee7e1491b0dda2bb44b0bfcaaac937947d7b6d81
metincontact <metinmemmedlicontact@gmail.com>
2026-04-21 23:42:42 +0200

    Enhance Header component with search and alt attributes
    Added search functionality and improved accessibility by including alt attributes for images.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
 import "./Header.css";
 import { Link } from "react-router";
 
-function Header({ cart }) {
+function Header({ cart, onSearch, searchQuery }) {
   let totalQuantity = 0;
-
   if (cart) {
     cart.forEach((cartItem) => {
       totalQuantity += cartItem.quantity;
@@ -15,16 +14,21 @@ function Header({ cart }) {
       <div className="header">
         <div className="left-section">
           <Link to="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
+            <img className="logo" src="images/logo-white.png" alt="Logo" />
+            <img className="mobile-logo" src="images/mobile-logo-white.png" alt="Logo" />
           </Link>
         </div>
 
         <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search"
+            value={searchQuery ?? ""}
+            onChange={(e) => onSearch && onSearch(e.target.value)}
+          />
           <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
+            <img className="search-icon" src="images/icons/search-icon.png" alt="Search" />
           </button>
         </div>
 
@@ -32,9 +36,8 @@ function Header({ cart }) {
           <Link className="orders-link header-link" to="/orders">
             <span className="orders-text">Orders</span>
           </Link>
-
           <Link className="cart-link header-link" to="/checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
+            <img className="cart-icon" src="images/icons/cart-icon.png" alt="Cart" />
             <div className="cart-quantity">{totalQuantity}</div>
             <div className="cart-text">Cart</div>
           </Link>
